93b5c287c267898367b088cb8472b50c05587885
Bookhunmee <ogunribidobukky@gmail.com>
2022-09-08 20:24:20 -0700

    preprocessor

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,25 +1,14 @@
 #include <stdio.h>
 
-
-
 /**
-
  * main - Entry point
-
  *
-
  *
-
  * Return: Always 0 (success)
-
  */
 
-
-
 int main(void)
-
 {
-
 	printf("Size of a char: %ld byte(s)\n", sizeof(char));
 
 	printf("Size of an int: %ld byte(s)\n", sizeof(int));
@@ -30,8 +19,5 @@ int main(void)
 
 	printf("Size of a float: %ld byte(s)\n", sizeof(float));
 
-
-
 	return (0);
-
 }
