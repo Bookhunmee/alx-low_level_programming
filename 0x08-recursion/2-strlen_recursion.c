c126fc4d6e558b31d2e5e93010d6ecfbb14fa652
Bookhunmee <ogunribidobukky@gmail.com>
2023-01-22 03:09:36 -0800

    message

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,19 +1,18 @@
-#include "holberton.h"
-
+#include "main.h"
 /**
- * _strlen_recursion - returns the length of a string
- * @s: pointer the string
- * Return: int
+ * _strlen_recursion - returns length of a string.
+ * @s: pointer the string.
+ *
+ * Return: lenght of string.
  */
-
 int _strlen_recursion(char *s)
 {
-	int n = 0;
-
-	if (*s > '\0')
+	if (*s)
 	{
-		n += _strlen_recursion(s + 1) + 1;
+		return (1 + _strlen_recursion(s + 1);
 	}
-
-	return (n);
+	else
+	{
+		return (0);
+	}
 }
